99b4d77208ff3aeebb9257eee80601da17aa4b7b
Momo Chan <60609841+monalisa282@users.noreply.github.com>
2020-10-22 12:08:03 +0530

    Add files via upload
    Do you wanna headache :p

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(800, 800);
+  
+  
+  background(170,255,0);
+}
+
+function draw() {
+  
+  
+  
+  background(random(170,255));
+  
+  noStroke()
+  fill(random(100,255) , random(170 , 20))
+  
+
+      ellipse(mouseX-100 , mouseY-100 ,mouseY ,mouseX);
+  
+    fill(random(255) , random( 20))
+  
+  
+
+  
+}
+
